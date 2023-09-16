7b4931a8b9266b152459477127a442b319f3645c
Jakob Johannes Bauer <private@jakobjbauer.com>
2023-09-16 15:17:50 +0200

    Added multiple keyword support

--- search.py
+++ search.py
@@ -1,12 +1,24 @@
 from common import PDF_INDEX, elasticsearch as es
 
 
-def search_pdf_text(keyword):
-    search_result = es.search(index=PDF_INDEX, body={'query': {'match': {'text': keyword}}})
+def search_pdf_text(keywords):
+    should_clauses = [{'match': {'text': keyword}} for keyword in keywords]
+    query = {
+        'bool': {
+            'should': should_clauses
+        }
+    }
+
+    # Perform the search using the standard Elasticsearch client
+    search_result = es.search(index=PDF_INDEX, body={'query': query})
     hits = search_result.get('hits', {}).get('hits', [])
     if not hits:
-        print(f"No hits on query {keyword}")
+        print(f"No hits on query {keywords}")
         return []
     for hit in hits:
         print(hit.get("_id", "Error: Malformatted response"))
     return list(map(lambda x: {"id": hit.get("_id"), "text": hit.get("_source").get("text")}, hits))
+
+
+def search_pdf_context(vector):
+    pass
