95dcbb1cedf33f8c3bdaaa49d63bd2efcbdd8761
Jan Leutwyler <jan.leutwyler.ruag@outlook.com>
2023-09-16 21:32:21 +0200

    test

--- vertex_ai.py
+++ vertex_ai.py
@@ -18,11 +18,11 @@ class VertexAI():
 
 
     def summarize_document(self, prompt, content):
-        prompt = f'Analyse the following question: \n{prompt} \nAnd write a short summary to the following text and put it in relation to the previous question. Text:\n{content}\nSummary:'
+        prompt = f'Analyse the following question:\n{prompt}\nAnd write a short summary to the following text and put it in relation to the previous question. Text:\n{content}\nSummary:'
         return self.execute_prompt(prompt)        
 
     def get_keywords(self, search_phrase):
-        prompt = f'From the phrase:\n{search_phrase}\nGenerate keywords for the following. Each keyword shall either be a single or composed term.\n Deliver the keywords as a comma-separated list of strings.' 
+        prompt = f'Generate new keywords from the phrase:\n{search_phrase}\nEach keyword shall either be a single or composed term.\nDeliver the keywords as a comma-separated list of strings.' 
         return self.execute_prompt(prompt)
 
     def execute_prompt(self, prompt):
