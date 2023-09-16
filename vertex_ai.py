7fcae136a40a4321fba9841c7e5e053ff6af3c2e
Jan Leutwyler <jan.leutwyler.ruag@outlook.com>
2023-09-16 21:28:46 +0200

    test

--- vertex_ai.py
+++ vertex_ai.py
@@ -22,7 +22,7 @@ class VertexAI():
         return self.execute_prompt(prompt)        
 
     def get_keywords(self, search_phrase):
-        prompt = f'Suggest new keywords for the following search terms:\n{search_phrase}\n Each new keyword shall either be a single or composed term.\n Deliver the suggestions as a comma-separated list of strings with the key \'suggestions\'.' 
+        prompt = f'From the phrase:\n{search_phrase}\nGenerate keywords for the following. Each keyword shall either be a single or composed term.\n Deliver the keywords as a comma-separated list of strings.' 
         return self.execute_prompt(prompt)
 
     def execute_prompt(self, prompt):
