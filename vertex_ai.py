e0f444bffc52e880581fd880b417707640f20226
kalotayz <zsombor.kalotay@gmx.ch>
2023-09-16 12:12:45 +0200

    fix bug for real for real

--- vertex_ai.py
+++ vertex_ai.py
@@ -6,32 +6,29 @@ import requests
 import json
 
 class VertexAI():
-    def __init__(self):
-        pass
-
-
-    def summarize_document(self, content):
-        prompt = f'Analyse the following text and write a summary:\n{content}\nSummary:'
-        print(prompt)
-        execute_prompt(prompt)
-
-        
-
-    def get_keywords(self, search_phrase):
-        prompt = f'You are responsible for managing a search engine of a company\'s knowledge base that is selling concrete and machinery to concrete. Users come to you with search phrases. Suggest new keywords to your user in a json format to following search terms:\n{search_phrase}. The suggestions shall be stored as a list of strings with the key \'suggestions\'.' 
-        print(prompt)
-        execute_prompt(prompt)
-
-    def execute_prompt(self, prompt):
+    def __init__(self):        
         vertexai.init(project="hackzurich23-8229", location="us-central1")
-        parameters = {
+        self.parameters = {
             "max_output_tokens": 1024,
             "temperature": 0.2,
             "top_p": 0.8,
             "top_k": 40
         }
-        model = TextGenerationModel.from_pretrained("text-bison-32k")
-        response = model.predict(
+        self.model = TextGenerationModel.from_pretrained("text-bison-32k")
+
+
+    def summarize_document(self, content):
+        prompt = f'Analyse the following text and write a summary:\n{content}\nSummary:'
+        print(prompt)
+        self.execute_prompt(prompt)        
+
+    def get_keywords(self, search_phrase):
+        prompt = f'You are responsible for managing a search engine of a company\'s knowledge base that is selling concrete and machinery to concrete. Users come to you with search phrases. Suggest new keywords to your user in a json format to following search terms:\n{search_phrase}. The suggestions shall be stored as a list of strings with the key \'suggestions\'.' 
+        print(prompt)
+        self.execute_prompt(prompt)
+
+    def execute_prompt(self, prompt):
+        response = self.model.predict(
             prompt,
             **parameters
         )
